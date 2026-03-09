0d04d130e70340fd37434ac6e505b0d7f21460f0
imantarico <imantarico@gmail.com>
2026-03-09 10:14:30 +0700

    menambahakan navbar

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+      <div class="container">
+    <h1>About</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In eaque possimus laborum autem commodi nisi ratione pariatur voluptates necessitatibus iusto?</p>
+      </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/gallery', function () {
+    return view('gallery');
+});
+Route::get('/about', function () {
+    return view('about');
+});
